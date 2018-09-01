a98a6a5605f79ca06b88016a7aa229ead346ce5f
KnowingDeWay <12886824@student.uts.edu.au>
2018-09-01 22:13:04 +1000

    Created sample navigation menu code for all to refer to

--- app/src/main/java/com/example/hqng/studio1b/MainActivity.java
+++ app/src/main/java/com/example/hqng/studio1b/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.hqng.studio1b;
 
+import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.sampleTb);
+        setSupportActionBar(mToolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.sampleDl);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = (NavigationView) findViewById(R.id.sampleNv);
+        navigationView.setNavigationItemSelectedListener(this);
+        setTitle("Your feature title here");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+
+        if(mToggle.onOptionsItemSelected(menuItem)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id  = item.getItemId();
+        //Plan here is to have all the home screens of all the other features in one project so that our navigation bar can access them
+        switch(id){
+
+        }
+        return false;
     }
 }
