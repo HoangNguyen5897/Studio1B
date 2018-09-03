f5b623e783b06f6cc29c3ebd752cd05ea3ca03ff
WUWEN <1528907497@qq.com>
2018-09-03 19:21:56 +1000

    upload image and change photo dialog

---
+++ app/src/main/java/com/example/hqng/studio1b/Init.java
@@ -0,0 +1,17 @@
+package com.example.hqng.studio1b;
+
+
+import android.Manifest;
+
+public class Init {
+    public Init(){
+
+    }
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA};
+    public static final int CAMER_REQUEST_CODE = 5;
+    public static final int PICFILE_REQUEST_CODE = 8;
+
+}
--- app/src/main/java/com/example/hqng/studio1b/MainActivity.java
+++ app/src/main/java/com/example/hqng/studio1b/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.hqng.studio1b;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Compress a bitmap by the @param "quality"
+     * Quality can be anywhere from 1 -100 : 100 being the highest quality.
+     * @param bitmap
+     * @param quality
+     * @return
+     */
+    public Bitmap compressBitmap(Bitmap bitmap, int quality){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+        return bitmap;
     }
 }
