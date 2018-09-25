8c868ecfb255e8c418c6019907f3eadbca50f560
adibmkhan <37283920+adibmkhan@users.noreply.github.com>
2018-09-25 10:55:16 +1000

    -Redesigned Register, Firebase is working for simple Auth. -Need to figure out how to store the attributes in firebase.
    -On my way to login

--- app/src/main/java/com/example/hqng/studio1b/PatientLogin.java
+++ app/src/main/java/com/example/hqng/studio1b/PatientLogin.java
@@ -5,14 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class PatientLogin extends AppCompatActivity {
+
+    private Button patientLoginButton;
+    private EditText patientLoginEmail;
+    private EditText patientLoginPassword;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login);
+
+        patientLoginEmail = (EditText) findViewById(R.id.patientLoginEmail);
+        patientLoginPassword = (EditText) findViewById(R.id.patientLoginPassword);
+        patientLoginButton = (Button) findViewById(R.id.patientLoginButton);
 
         getSupportActionBar().setTitle("Patient Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
