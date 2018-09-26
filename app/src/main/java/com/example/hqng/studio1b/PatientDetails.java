49eef8c3d0b0550b50027b25258fc9dcba263dbe
adibmkhan <37283920+adibmkhan@users.noreply.github.com>
2018-09-26 13:39:11 +1000

    - Database is being updated. - Login logout working
    - Issues while registering new users!
    - Issues with login with old accounts, which was registered before today

--- app/src/main/java/com/example/hqng/studio1b/PatientDetails.java
+++ app/src/main/java/com/example/hqng/studio1b/PatientDetails.java
@@ -5,16 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PatientDetails extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
 
-    private Button logoutButton;
-    private Button patientSaveButton;
+
+
+    private DatabaseReference databaseReference;
+
+    private EditText regSurname, regOtherName, regGender, regAge, regHeight, regWeight, regAddress;
+    private Button logoutButton, patientSaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +36,41 @@ public class PatientDetails extends AppCompatActivity implements View.OnClickLis
             startActivity(new Intent(this, PatientLogin.class));
         }
 
-        FirebaseUser user = firebaseAuth.getCurrentUser();
+        databaseReference= FirebaseDatabase.getInstance().getReference();
 
+        regSurname = (EditText) findViewById(R.id.regSurname);
+        regOtherName = (EditText) findViewById(R.id.regOtherName);
+        regGender = (EditText) findViewById(R.id.regGender);
+        regAge = (EditText) findViewById(R.id.regAge);
+        regHeight = (EditText) findViewById(R.id.regHeight);
+        regWeight = (EditText) findViewById(R.id.regWeight);
+        regAddress = (EditText) findViewById(R.id.regAddress);
+        patientSaveButton = (Button) findViewById(R.id.patientSaveButton);
         logoutButton = (Button) findViewById(R.id.logoutButton);
 
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
         logoutButton.setOnClickListener(this);
+        patientSaveButton.setOnClickListener(this);
+    }
+
+    private void savePatientInformation(){
+
+        String surname = regSurname.getText().toString().trim();
+        String otherName = regOtherName.getText().toString().trim();
+        String gender = regGender.getText().toString().trim();
+        String age = regAge.getText().toString().trim();
+        String height = regHeight.getText().toString().trim();
+        String weight = regWeight.getText().toString().trim();
+        String address = regAddress.getText().toString().trim();
+
+        PatientInformation patientInformation = new PatientInformation(surname,otherName,gender,age,height,weight,address);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(patientInformation);
+
+        Toast.makeText(this, "Patient details saved...", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -42,6 +80,11 @@ public class PatientDetails extends AppCompatActivity implements View.OnClickLis
             firebaseAuth.signOut();
             finish();
             startActivity(new Intent( this, PatientLogin.class));
+        }
+
+        if(view == patientSaveButton){
+
+            savePatientInformation();
         }
 
 
