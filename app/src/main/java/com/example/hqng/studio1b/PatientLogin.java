6fe275e89d7b40f6697b3a7f492c1334c2e7ccd3
adibmkhan <37283920+adibmkhan@users.noreply.github.com>
2018-09-26 11:12:37 +1000

    -Login Done, how to integrate profile when login is clicked -Need to figure out how to store the attributes in firebase.

--- app/src/main/java/com/example/hqng/studio1b/PatientLogin.java
+++ app/src/main/java/com/example/hqng/studio1b/PatientLogin.java
@@ -1,17 +1,30 @@
 package com.example.hqng.studio1b;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class PatientLogin extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class PatientLogin extends AppCompatActivity implements View.OnClickListener {
 
     private Button patientLoginButton;
     private EditText patientLoginEmail;
     private EditText patientLoginPassword;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -19,12 +32,73 @@ public class PatientLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+
+            //profile activity here
+            //Simplified coding, part 2, around 11 minutes
+        }
+
         patientLoginEmail = (EditText) findViewById(R.id.patientLoginEmail);
         patientLoginPassword = (EditText) findViewById(R.id.patientLoginPassword);
         patientLoginButton = (Button) findViewById(R.id.patientLoginButton);
 
+        patientLoginButton.setOnClickListener(this);
+
+
+
+        progressDialog = new ProgressDialog(this);
+
+
         getSupportActionBar().setTitle("Patient Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    private void patientLogin(){
+
+        String email = patientLoginEmail.getText().toString().trim();
+        String password = patientLoginPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            //stopping function from executing further
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            //password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            //stopping function from executing further
+            return;
+        }
+
+        progressDialog.setMessage("Logging in");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+
+                        if(task.isSuccessful()){
+                            //start the profile activity
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == patientLoginButton){
+            patientLogin();
+
+        }
 
 
     }
